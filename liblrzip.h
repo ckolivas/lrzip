00be150cfb22639d3d35d26109542ec97d7afe83
Con Kolivas <kernel@kolivas.org>
2011-03-14 14:56:59 +1100

    Add flag tests for TMP_INBUF.

--- liblrzip.h
+++ liblrzip.h
@@ -47,6 +47,7 @@
 #define KEEP_BROKEN	(control->flags & FLAG_KEEP_BROKEN)
 #define LZO_TEST	(control->flags & FLAG_THRESHOLD)
 #define TMP_OUTBUF	(control->flags & FLAG_TMP_OUTBUF)
+#define TMP_INBUF	(control->flags & FLAG_TMP_INBUF)
 
 #define print_output(format, args...)	do {\
 	fprintf(control->msgout, format, ##args);	\
