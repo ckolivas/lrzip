6ddd2e8e7e5ce7104d252ba52600abe2ca435707
ckolivas <kernel@kolivas.org>
2011-04-18 23:13:11 +1000

    Correct fix to ensure 64 bit lseek is used on apple in stream.c

--- stream.c
+++ stream.c
@@ -27,8 +27,14 @@
 #ifdef HAVE_SYS_TIME_H
 # include <sys/time.h>
 #endif
+#ifdef HAVE_SYS_TYPES_H
+# include <sys/types.h>
+#endif
 #ifdef HAVE_SYS_RESOURCE_H
 # include <sys/resource.h>
+#endif
+#ifdef HAVE_UNISTD_H
+# include <unistd.h>
 #endif
 #include <sys/statvfs.h>
 #include <pthread.h>
