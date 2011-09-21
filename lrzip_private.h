a28def8d05eaa41455abb28092843de6bab736f9
Con Kolivas <kernel@kolivas.org>
2011-09-21 16:38:00 +1000

    Mremap is linux only, so reverse the check for its presence and use fake mremap everywhere else.

--- lrzip_private.h
+++ lrzip_private.h
@@ -90,8 +90,7 @@ typedef uint32_t u32;
 typedef struct rzip_control rzip_control;
 typedef struct md5_ctx md5_ctx;
 
-#if defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || \
-	defined(__APPLE__) || defined(__CYGWIN__)
+#if !defined(__linux)
  #define mremap fake_mremap
 #endif
 
