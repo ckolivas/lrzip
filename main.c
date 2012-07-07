0d6944bd3f8cb19f7e4867f979ba753cd7e11110
Con Kolivas <kernel@kolivas.org>
2012-07-07 21:02:35 +1000

    Fix invalid free error when -o and -S are used together.

--- main.c
+++ main.c
@@ -281,13 +281,11 @@ int main(int argc, char *argv[])
 				failure("Invalid nice value (must be -20..19)\n");
 			break;
 		case 'o':
-			if (control->outdir)
-				failure("Cannot have -o and -O together\n");
+			if (control->outdir || control->suffix)
+				failure("Cannot have -o and -O or -S together\n");
 			if (unlikely(STDOUT))
 				failure("Cannot specify an output filename when outputting to stdout\n");
 			control->outname = optarg;
-			free(control->suffix);
-			control->suffix = "";
 			break;
 		case 'O':
 			if (control->outname)	/* can't mix -o and -O */
