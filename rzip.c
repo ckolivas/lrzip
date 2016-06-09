56a84987dd1bdaf0b9cd98c53c50685c82159240
Con Kolivas <kernel@kolivas.org>
2016-06-09 11:16:41 +1000

    Update copyright dates

--- rzip.c
+++ rzip.c
@@ -1,5 +1,5 @@
 /*
-   Copyright (C) 2006-2015 Con Kolivas
+   Copyright (C) 2006-2016 Con Kolivas
    Copyright (C) 1998 Andrew Tridgell
 
    Modified to use flat hash, memory limit and variable hash culling
