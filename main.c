f9b127cd5a542ee85866b000f1a124be8cac5bd2
Peter Hyman <pete@peterhyman.com>
2020-05-28 10:26:58 -0500

    BUGFIX: lrzip -d, -t should allow a file with or without an lrz extension and add it if necessary

--- main.c
+++ main.c
@@ -594,7 +594,8 @@ int main(int argc, char *argv[])
 			infile = argv[i];
 		else if (!(i == 0 && STDIN))
 			break;
-		if (infile) {
+		if (infile && !(DECOMPRESS || TEST_ONLY)) {
+			/* check that input file exists, unless Decompressing or Test */
 			if ((strcmp(infile, "-") == 0))
 				control->flags |= FLAG_STDIN;
 			else {
