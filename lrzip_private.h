884afda7772c960e18e7f301e75c946679e448b4
rubenllorente <108059858+rubenllorente@users.noreply.github.com>
2022-06-26 09:36:44 +0000

    Create lrzip_private.h
    Fix defining the builtin_alloca for gcc which we missed in testing

--- lrzip_private.h
+++ lrzip_private.h
@@ -48,6 +48,8 @@
 # include <alloca.h>
 #elif defined _AIX
 # define alloca __alloca
+#elif defined __GNUC__
+# define alloca __builtin_alloca
 #elif defined _MSC_VER
 # include <malloc.h>
 # define alloca _alloca
