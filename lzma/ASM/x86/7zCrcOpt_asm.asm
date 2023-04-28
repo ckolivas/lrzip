0c6f6b5723cbb746790ae7186f4b5f3048d93470
Bernhard M. Wiedemann <githubbmw2020@lsmod.de>
2023-04-28 02:25:04 +0200

    Avoid executable stack (#243)
    without this patch, the rpmlint check for executable stack
    failed the build on openSUSE.

--- lzma/ASM/x86/7zCrcOpt_asm.asm
+++ lzma/ASM/x86/7zCrcOpt_asm.asm
@@ -138,4 +138,10 @@ MY_ENDP
 %ifidn __OUTPUT_FORMAT__,elf
 section .note.GNU-stack noalloc noexec nowrite progbits
 %endif
+%ifidn __OUTPUT_FORMAT__,elf32
+section .note.GNU-stack noalloc noexec nowrite progbits
+%endif
+%ifidn __OUTPUT_FORMAT__,elf64
+section .note.GNU-stack noalloc noexec nowrite progbits
+%endif
 
